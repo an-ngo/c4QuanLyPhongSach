383ce5012055da3b85ce09bea44a6cb3540b61d6
An <phowflowf135@gmail.com>
2021-12-14 15:51:13 +0700

    prepare

---
+++ src/main/java/com/team5/c4quanlyphongsach/C4QuanLyPhongSachApplication.java
@@ -0,0 +1,13 @@
+package com.team5.c4quanlyphongsach;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class C4QuanLyPhongSachApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(C4QuanLyPhongSachApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/team5/c4quanlyphongsach/C4QuanLyPhongSachApplicationTests.java
@@ -0,0 +1,13 @@
+package com.team5.c4quanlyphongsach;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class C4QuanLyPhongSachApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
