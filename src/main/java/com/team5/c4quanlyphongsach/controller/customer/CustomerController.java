1d58924e118131baa84ffa952fb16abb8c5a0214
Nguyễn Tiến Long <email ng k git>
2021-12-16 16:16:15 +0700

    Bt

--- src/main/java/com/team5/c4quanlyphongsach/controller/customer/CustomerController.java
+++ src/main/java/com/team5/c4quanlyphongsach/controller/customer/CustomerController.java
@@ -16,21 +16,8 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.util.FileCopyUtils;
-import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
-<<<<<<< HEAD
-import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.multipart.MultipartFile;
-=======
-<<<<<<< HEAD
-import org.springframework.web.multipart.MultipartFile;
-
-=======
-import org.springframework.web.servlet.ModelAndView;
-import org.springframework.web.multipart.MultipartFile;
->>>>>>> 42357c43f4c9aece782e0d06d4906db540f77bc4
->>>>>>> 657c88b3ac72559ab0c6a035b4b321e68c211923
-
 import javax.servlet.http.HttpSession;
 import javax.validation.Valid;
 import java.io.File;
